ab26ebd14cd24f8451b783d15ed4eeb481956f68
Brijen Makwana <brijenma@gmail.com>
2023-08-01 15:41:07 +0530

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, View } from "react-native";
+
+export default function TabOneScreen() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});

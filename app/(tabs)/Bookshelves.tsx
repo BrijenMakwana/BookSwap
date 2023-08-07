acb5b2b24e02afde635b9c86f98f778028cedd75
Brijen Makwana <brijenma@gmail.com>
2023-08-07 11:00:40 +0530

    search tab

---
+++ app/(tabs)/Bookshelves.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Bookshelves = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Bookshelves</Text>
+    </View>
+  );
+};
+
+export default Bookshelves;
+
+const styles = StyleSheet.create({
+  container: {},
+});
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,85 +1,14 @@
-import {
-  FlatList,
-  SafeAreaView,
-  StyleSheet,
-  RefreshControl,
-} from "react-native";
-import SearchBar from "@/components/SearchBar";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import Book from "@/components/Book";
-import { FlashList } from "@shopify/flash-list";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-export default function TabOneScreen() {
-  const [searchedBook, setSearchedBook] = useState<string>("");
-  const [isSearching, setIsSearching] = useState<boolean>(false);
-
-  const [books, setBooks] = useState([]);
-
-  const searchBooks = async () => {
-    setIsSearching(true);
-    try {
-      const response = await axios.get(
-        `https://www.googleapis.com/books/v1/volumes?q=${searchedBook}&key=${process.env.EXPO_PUBLIC_API_KEY}`
-      );
-      setBooks(response.data.items);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsSearching(false);
-    }
-  };
-
-  useEffect(() => {
-    if (!searchedBook) {
-      setBooks([]);
-      return;
-    }
-
-    const searchBooksTimeOut = setTimeout(() => {
-      searchBooks();
-    }, 1500);
-
-    return () => clearTimeout(searchBooksTimeOut);
-  }, [searchedBook]);
-
+const index = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <SearchBar
-        searchedBook={searchedBook}
-        setSearchedBook={setSearchedBook}
-      />
-
-      <FlashList
-        data={books}
-        renderItem={({ item }) => (
-          <Book
-            id={item.id}
-            imageUrl={item?.volumeInfo?.imageLinks?.smallThumbnail}
-            title={item?.volumeInfo?.title}
-            overview={item?.searchInfo?.textSnippet}
-            author={item?.volumeInfo?.authors[0] || "NA"}
-          />
-        )}
-        keyExtractor={(item) => item.id}
-        showsVerticalScrollIndicator={false}
-        estimatedItemSize={20}
-        refreshControl={
-          <RefreshControl
-            refreshing={isSearching}
-            colors={["#e0218a"]}
-            progressBackgroundColor="#fff"
-            onRefresh={() => searchBooks()}
-          />
-        }
-      />
-    </SafeAreaView>
+    <View>
+      <Text>index</Text>
+    </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-});
+export default index;
+
+const styles = StyleSheet.create({});

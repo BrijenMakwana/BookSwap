a7f7c8f093cf6013217058d8006f2edf2f7f8ac1
Brijen Makwana <brijenma@gmail.com>
2023-08-19 10:30:32 +0530

    request book btn in BookRequest

--- components/RequestBook.tsx
+++ components/RequestBook.tsx
@@ -1,9 +1,43 @@
-import { StyleSheet, ToastAndroid } from "react-native";
+import {
+  Pressable,
+  StyleSheet,
+  ToastAndroid,
+  useColorScheme,
+  ColorSchemeName,
+  Linking,
+} from "react-native";
 import { useEffect, useState } from "react";
 import Book from "./Book";
 import axios from "axios";
+import { Entypo } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+
+const RequestBtn = (props) => {
+  const colorScheme: ColorSchemeName = useColorScheme();
+
+  const { onPress } = props;
+
+  return (
+    <Pressable
+      style={[
+        styles.requestBtn,
+        {
+          backgroundColor: Colors[colorScheme].barbie,
+        },
+      ]}
+      onPress={onPress}
+      android_ripple={{
+        color: Colors[colorScheme].background,
+      }}
+    >
+      <Entypo name="forward" size={20} color={Colors[colorScheme].background} />
+    </Pressable>
+  );
+};
 
 const RequestBook = (props) => {
+  const colorScheme: ColorSchemeName = useColorScheme();
+
   const [book, setBook] = useState({});
 
   const { bookID } = props;
@@ -19,23 +53,56 @@ const RequestBook = (props) => {
     }
   };
 
+  const sendEmailRequest = () => {
+    const subject = "Your subject here";
+    const senderEmail = "your@email.com";
+    const emailBody = "Your email body text here";
+
+    const emailUrl = `mailto:${encodeURIComponent(
+      senderEmail
+    )}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(
+      emailBody
+    )}`;
+
+    Linking.canOpenURL(emailUrl)
+      .then((supported) => {
+        if (!supported) {
+          console.log("Email app is not supported on this device");
+        } else {
+          return Linking.openURL(emailUrl);
+        }
+      })
+      .catch((error) => console.error("Error opening email app:", error));
+  };
+
   useEffect(() => {
     getBook();
   }, []);
 
   return (
-    <Book
-      id={book.id}
-      imageUrl={book?.volumeInfo?.imageLinks?.thumbnail}
-      title={book?.volumeInfo?.title}
-      author={book?.volumeInfo?.authors[0] || "NA"}
-      rating={book?.volumeInfo?.averageRating}
-      publishedDate={book.volumeInfo?.publishedDate}
-      pageCount={book.volumeInfo?.pageCount}
-    />
+    <>
+      <Book
+        id={book.id}
+        imageUrl={book?.volumeInfo?.imageLinks?.thumbnail}
+        title={book?.volumeInfo?.title}
+        author={book?.volumeInfo?.authors[0] || "NA"}
+        rating={book?.volumeInfo?.averageRating}
+        publishedDate={book.volumeInfo?.publishedDate}
+        pageCount={book.volumeInfo?.pageCount}
+      />
+      <RequestBtn onPress={sendEmailRequest} />
+    </>
   );
 };
 
 export default RequestBook;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  requestBtn: {
+    position: "absolute",
+    top: 15,
+    left: 15,
+    padding: 10,
+    borderRadius: 5,
+  },
+});
